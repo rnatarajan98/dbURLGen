635d12f89c3373cfe422b1b9d3e08bd5115ab768
Richard <richard.natarajan.98@gmail.com>
2019-12-27 21:53:39 +0000

    Initial commit

---
+++ src/main/java/dbURLGenServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns={},loadOnStartup = 1)
+public class dbURLGenServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        resp.setContentType("text/html");
+        resp.getWriter().write("Hello World!");
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException {
+    }
+
+}
